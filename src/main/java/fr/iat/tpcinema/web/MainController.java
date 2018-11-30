820376b1737e533279606de474afc41f524b4dc0
IamTerror <32469633+IamTerror@users.noreply.github.com>
2018-11-30 11:12:54 +0100

    page detail done

--- src/main/java/fr/iat/tpcinema/web/MainController.java
+++ src/main/java/fr/iat/tpcinema/web/MainController.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class MainController {
@@ -19,4 +24,19 @@ public class MainController {
         return "index";
     }
 
+    // Version avec @RequestParam
+//    @GetMapping("/film")
+//    public String detail(Model model, @RequestParam("id") String id) {
+//        Integer idFilm = Integer.parseInt(id);
+//        model.addAttribute("film", filmsDao.getById(idFilm));
+//        return "detail";
+//    }
+
+    // Version avec @PathVariable
+    @GetMapping("/film/{id}")
+    public String detail(Model model, @PathVariable("id") String id) {
+        Integer idFilm = Integer.parseInt(id);
+        model.addAttribute("film", filmsDao.getById(idFilm));
+        return "detail";
+    }
 }
