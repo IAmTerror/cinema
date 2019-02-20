64b15e392e97109c0d7109c5440e902e76a76dcb
IAmTerror <cyrilberthout@gmail.com>
2019-02-20 08:52:51 +0100

    Remove old model implementations done Refactoring Review ok

--- src/main/java/fr/iat/cinema/model/Person.java
+++ src/main/java/fr/iat/cinema/model/Person.java
@@ -1,120 +1,3 @@
-//package fr.iat.cinema.model;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//import java.util.Objects;
-//
-//public class Person {
-//    private int id;
-//    private String nom;
-//    private String prenom;
-//    private Integer naissance;
-//    private String affiche;
-//    private int type;  // 1 = acteur ; 2 = réalisateur
-//    private List<Role> roles = new ArrayList<>();
-//
-//    public Person(int id, String nom, String prenom, Integer naissance, String affiche, int type) {
-//        this.id = id;
-//        this.nom = nom;
-//        this.prenom = prenom;
-//        this.naissance = naissance;
-//        this.affiche = affiche;
-//        this.type = type;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public void setId(int id) {
-//        this.id = id;
-//    }
-//
-//    public String getNom() {
-//        return nom;
-//    }
-//
-//    public void setNom(String nom) {
-//        this.nom = nom;
-//    }
-//
-//    public String getPrenom() {
-//        return prenom;
-//    }
-//
-//    public void setPrenom(String prenom) {
-//        this.prenom = prenom;
-//    }
-//
-//    public Integer getNaissance() {
-//        return naissance;
-//    }
-//
-//    public void setNaissance(Integer naissance) {
-//        this.naissance = naissance;
-//    }
-//
-//    public String getAffiche() {
-//        return affiche;
-//    }
-//
-//    public void setAffiche(String affiche) {
-//        this.affiche = affiche;
-//    }
-//
-//    public int getType() {
-//        return type;
-//    }
-//
-//    public void setType(int type) {
-//        this.type = type;
-//    }
-//
-//    public List<Role> getRoles() {
-//        return roles;
-//    }
-//
-//    public void setRoles(List<Role> roles) {
-//        this.roles = roles;
-//    }
-//
-//    public void addRole(Role role){
-//        if (!roles.contains(role)) {
-//            this.roles.add(role);
-//        }
-//    }
-//
-//    @Override
-//    public boolean equals(Object o) {
-//        if (this == o) return true;
-//        if (!(o instanceof Person)) return false;
-//        Person person = (Person) o;
-//        return getId() == person.getId() &&
-//                Objects.equals(getNom(), person.getNom()) &&
-//                Objects.equals(getPrenom(), person.getPrenom()) &&
-//                Objects.equals(getNaissance(), person.getNaissance()) &&
-//                Objects.equals(getAffiche(), person.getAffiche()) &&
-//                Objects.equals(roles, person.roles);
-//    }
-//
-//    @Override
-//    public int hashCode() {
-//        return Objects.hash(getId(), getNom(), getPrenom(), getNaissance(), getAffiche(), roles);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Person{" +
-//                "id=" + id +
-//                ", nom='" + nom + '\'' +
-//                ", prenom='" + prenom + '\'' +
-//                ", naissance=" + naissance +
-//                ", affiche='" + affiche + '\'' +
-//                ", roles=" + roles +
-//                '}';
-//    }
-//}
-
 package fr.iat.cinema.model;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
@@ -224,14 +107,13 @@ public class Person {
         if (!(o instanceof Person)) return false;
         Person person = (Person) o;
         return getId() == person.getId() &&
-                getIdtmdb() == person.getIdtmdb() &&
+                Objects.equals(getIdtmdb(), person.getIdtmdb()) &&
                 Objects.equals(getName(), person.getName()) &&
-                Objects.equals(getBirthday(), person.getBirthday()) &&
-                Objects.equals(getImagePath(), person.getImagePath());
+                Objects.equals(getBirthday(), person.getBirthday());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getId(), getIdtmdb(), getName(), getBirthday(), getImagePath());
+        return Objects.hash(getId(), getIdtmdb(), getName(), getBirthday());
     }
 }
--- src/main/java/fr/iat/cinema/model/Review.java
+++ src/main/java/fr/iat/cinema/model/Review.java
@@ -1,23 +1,224 @@
+// ===== OLD IMPLEMENTATION ============================================================================================
+
+//package fr.iat.cinema.model;
+//
+//import javax.persistence.*;
+//import java.sql.Timestamp;
+//
+//@Entity
+//@Table(name = "review")
+//public class Review {
+//
+//
+//    public static final int WAITING_MODERATION = 1;
+//    public static final int PUBLISHED = 2;
+//    public static final int MUST_BE_MODIFIED = 3 ;
+//    public static final int DELETED = 4;
+//    public static final int ABANDONED = 5;
+//    public static final int REJECTED = 6 ;
+//
+//    private long state = Review.WAITING_MODERATION;
+//
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    @Column(name = "id", nullable = false)
+//    private long id;
+//    @Basic
+//    @Column(name = "article", nullable = true, length = -1)
+//    private String article;
+//    @Basic
+//    @Column(name = "datte", nullable = false)
+//    private Timestamp date;
+//    @ManyToOne
+//    @JoinColumn(name="film_id")
+//    private Film film;
+//    @ManyToOne
+//    @JoinColumn(name="user_id")
+//    private User user;
+//
+//
+//    public long getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+//
+//    public String getArticle() {
+//        return article;
+//    }
+//
+//    public void setArticle(String article) {
+//        this.article = article;
+//    }
+//
+//    public Timestamp getDate() {
+//        return date;
+//    }
+//
+//    public void setDate(Timestamp datte) {
+//        this.date = datte;
+//    }
+//
+//    public Film getFilm() {
+//        return film;
+//    }
+//
+//    public void setFilm(Film film) {
+//        this.film = film;
+//    }
+//
+//    public User getUser() {
+//        return user;
+//    }
+//
+//    public void setUser(User user) {
+//        this.user = user;
+//    }
+//
+//    public long getState() {
+//        return this.state;
+//    }
+//
+//    private boolean canTransitTo(int targetState) {
+//        boolean result;
+//        switch (targetState) {
+//            case Review.REJECTED :
+//                result = this.getState() == Review.WAITING_MODERATION;
+//                break;
+//            case Review.DELETED :
+//                result = this.getState() == Review.PUBLISHED;
+//                break;
+//            case Review.WAITING_MODERATION :
+//                result = this.getState() == Review.PUBLISHED || this.getState() == Review.WAITING_MODERATION
+//                        || this.getState() == Review.MUST_BE_MODIFIED;
+//                break;
+//            case Review.PUBLISHED :
+//                result = this.getState() == Review.WAITING_MODERATION;
+//                break;
+//            case Review.MUST_BE_MODIFIED :
+//                result = this.getState() == Review.WAITING_MODERATION;
+//                break;
+//            case Review.ABANDONED :
+//                result = this.getState() == Review.MUST_BE_MODIFIED;
+//                break;
+//
+//                default:
+//                    result = false;
+//        }
+//        return result;
+//    }
+//
+//    public void transitTo(int target) throws IllegalTransitionStateException {
+//        if(canTransitTo(target)) {
+//            this.state = target;
+//        } else {
+//            throw new IllegalTransitionStateException("Transition non autorisée");
+//        }
+//    }
+//
+//    public void validByModerator() throws IllegalTransitionStateException {
+//        transitTo(Review.PUBLISHED);
+//    }
+//
+//    public void keepForEditByModerator() throws IllegalTransitionStateException {
+//        transitTo(Review.MUST_BE_MODIFIED);
+//    }
+//
+//    public void deleteByUser() throws IllegalTransitionStateException {
+//        transitTo(Review.DELETED);
+//    }
+//
+//    public void abandonByUser() throws IllegalTransitionStateException {
+//        transitTo(Review.ABANDONED);
+//    }
+//
+//    public void rejectedByModerator() throws IllegalTransitionStateException {
+//        transitTo(Review.REJECTED);
+//    }
+//
+//    public void editByUser() throws IllegalTransitionStateException {
+//        transitTo(Review.WAITING_MODERATION);
+//    }
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//
+//        Review review = (Review) o;
+//
+//        if (id != review.id) return false;
+//        if (article != null ? !article.equals(review.article) : review.article != null) return false;
+//        if (date != null ? !date.equals(review.date) : review.date != null) return false;
+//
+//        return true;
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        int result = (int) (id ^ (id >>> 32));
+//        result = 31 * result + (article != null ? article.hashCode() : 0);
+//        result = 31 * result + (date != null ? date.hashCode() : 0);
+//        return result;
+//    }
+//}
+
+
+// ===== NEW IMPLEMENTATION ============================================================================================
+
 package fr.iat.cinema.model;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.*;
 
 @Entity
-@Table(name = "review")
 public class Review {
 
-    // TODO : faire un PlantUML du cycle de vie des commentaires
-    // TODO : récupérer la structure en hashmap du prof et implémenter
+    private static final Map<Integer, List<Integer>> transitionsTo;
+//    private static final Map<Integer, List<Integer>> transitions;
+
+
+    static {
+        // Transitions possibles
+        transitionsTo = new HashMap<>();
+        transitionsTo.put(Review.WAITING_MODERATION,
+                Arrays.asList(new Integer[]{Review.PUBLISHED,
+                        Review.WAITING_MODERATION,
+                        Review.MUST_BE_MODIFIED,
+                        Review.REJECTED
+                }));
+        transitionsTo.put(Review.PUBLISHED, Arrays.asList(new Integer[]{Review.WAITING_MODERATION, Review.DELETED}));
+        transitionsTo.put(Review.MUST_BE_MODIFIED, Arrays.asList(new Integer[]{Review.WAITING_MODERATION, Review.ABANDONED}));
+        // Trransitions impossibles
+        transitionsTo.put(Review.ABANDONED, new ArrayList<>());
+        transitionsTo.put(Review.DELETED, new ArrayList<>());
+        transitionsTo.put(Review.REJECTED, new ArrayList<>());
+    }
+
+//    static {
+//
+//        transitions = new HashMap<>();
+//        transitions.put(Review.REJECTED, Arrays.asList(new Integer[]{Review.WAITING_MODERATION}));
+//        transitions.put(Review.DELETED, Arrays.asList(new Integer[]{Review.PUBLISHED}));
+//        transitions.put(Review.WAITING_MODERATION,
+//                Arrays.asList(new Integer[]{Review.PUBLISHED,
+//                        Review.WAITING_MODERATION,
+//                        Review.MUST_BE_MODIFIED
+//                }));
+//        transitions.put(Review.PUBLISHED, Arrays.asList(new Integer[]{Review.WAITING_MODERATION}));
+//        transitions.put(Review.MUST_BE_MODIFIED, Arrays.asList(new Integer[]{Review.WAITING_MODERATION}));
+//        transitions.put(Review.ABANDONED, Arrays.asList(new Integer[]{Review.MUST_BE_MODIFIED}));
+//    }
 
     public static final int WAITING_MODERATION = 1;
     public static final int PUBLISHED = 2;
-    public static final int MUST_BE_MODIFIED = 3 ;
+    public static final int MUST_BE_MODIFIED = 3;
     public static final int DELETED = 4;
     public static final int ABANDONED = 5;
     public static final int REJECTED = 6 ;
-
-    private long state = Review.WAITING_MODERATION;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,12 +231,13 @@ public class Review {
     @Column(name = "datte", nullable = false)
     private Timestamp date;
     @ManyToOne
-    @JoinColumn(name="film_id")
+    @JoinColumn(name = "film_id")
     private Film film;
     @ManyToOne
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private User user;
 
+    private int state = Review.WAITING_MODERATION;
 
     public long getId() {
         return id;
@@ -52,6 +254,7 @@ public class Review {
     public void setArticle(String article) {
         this.article = article;
     }
+
 
     public Timestamp getDate() {
         return date;
@@ -77,71 +280,6 @@ public class Review {
         this.user = user;
     }
 
-    public long getState() {
-        return this.state;
-    }
-
-    private boolean canTransitTo(int targetState) {
-        boolean result;
-        switch (targetState) {
-            case Review.REJECTED :
-                result = this.getState() == Review.WAITING_MODERATION;
-                break;
-            case Review.DELETED :
-                result = this.getState() == Review.PUBLISHED;
-                break;
-            case Review.WAITING_MODERATION :
-                result = this.getState() == Review.PUBLISHED || this.getState() == Review.WAITING_MODERATION
-                        || this.getState() == Review.MUST_BE_MODIFIED;
-                break;
-            case Review.PUBLISHED :
-                result = this.getState() == Review.WAITING_MODERATION;
-                break;
-            case Review.MUST_BE_MODIFIED :
-                result = this.getState() == Review.WAITING_MODERATION;
-                break;
-            case Review.ABANDONED :
-                result = this.getState() == Review.MUST_BE_MODIFIED;
-                break;
-
-                default:
-                    result = false;
-        }
-        return result;
-    }
-
-    public void transitTo(int target) throws IllegalTransitionStateException {
-        if(canTransitTo(target)) {
-            this.state = target;
-        } else {
-            throw new IllegalTransitionStateException("Transition non autorisée");
-        }
-    }
-
-    public void validByModerator() throws IllegalTransitionStateException {
-        transitTo(Review.PUBLISHED);
-    }
-
-    public void keepForEditByModerator() throws IllegalTransitionStateException {
-        transitTo(Review.MUST_BE_MODIFIED);
-    }
-
-    public void deleteByUser() throws IllegalTransitionStateException {
-        transitTo(Review.DELETED);
-    }
-
-    public void abandonByUser() throws IllegalTransitionStateException {
-        transitTo(Review.ABANDONED);
-    }
-
-    public void rejectedByModerator() throws IllegalTransitionStateException {
-        transitTo(Review.REJECTED);
-    }
-
-    public void editByUser() throws IllegalTransitionStateException {
-        transitTo(Review.WAITING_MODERATION);
-    }
-
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -163,4 +301,51 @@ public class Review {
         result = 31 * result + (date != null ? date.hashCode() : 0);
         return result;
     }
+
+    public int getState() {
+        return this.state;
+    }
+
+    public void validByModerator() throws IllegalTransitionStateException {
+        transitTo(Review.PUBLISHED);
+    }
+
+    public void keepForEditByModerator() throws IllegalTransitionStateException {
+        transitTo(Review.MUST_BE_MODIFIED);
+    }
+
+    public void abandonByUser() throws IllegalTransitionStateException {
+        transitTo(Review.ABANDONED);
+    }
+
+    public void rejectedByModerator() throws IllegalTransitionStateException {
+        transitTo(Review.REJECTED);
+    }
+
+    public void editByUser() throws IllegalTransitionStateException {
+        transitTo(Review.WAITING_MODERATION);
+    }
+
+    public void deleteByUser() throws IllegalTransitionStateException {
+        transitTo(Review.DELETED);
+    }
+
+    private void transitTo(int target) throws IllegalTransitionStateException {
+        if (canTransitTo(target)) {
+            this.state = target;
+        } else {
+            throw new IllegalTransitionStateException("Transition non autorisée");
+        }
+    }
+
+    private boolean canTransitTo(int targetState) {
+
+//        return Review.transitions.get(targetState).contains(this.getState());
+
+        return Review.transitionsTo.get(this.getState()).contains(targetState);
+
+
+
+    }
+
 }
